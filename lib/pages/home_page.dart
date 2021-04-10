de93ae363cee50897c348b4f58f7f3db49d05951
charki <you@example.com>
2021-04-10 18:37:38 +0200

    add some change

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:villa52/pages/dashboard_page.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List<Widget> widgetList = [
+    DashboardPage(),
+    DashboardPage(),
+    DashboardPage(),
+    DashboardPage()
+  ];
+  int _index = 0;
+
+  @override
+  Wirdget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppBar(),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [],
+        selectedItemColor: Colors.redAccent,
+        unselectedItemColor: Colors.black,
+        type: BottomNavigationBarType.shifting,
+        currentIndex: _index,
+        onTap: (index) {
+          setState(() {
+            _index = index;
+          });
+        },
+      ),
+      body: _widgetList[_index],
+    );
+  }
+
+  Widget _buildAppBar() {
+    return AppBar(
+      centerTitle: true,
+      brightness: Brightness.dark,
+      elevation: 0,
+      backgroundColor: Colors.redAccent,
+      automaticallyImplyLeading: false,
+      title: Text(
+        'Villa52 App',
+        style: TextStyle(color: Colors.white),
+      ),
+      actions: [
+        Icon(
+          Icons.notifications_none,
+          color: Colors.white,
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Icon(
+          Icons.shopping_cart,
+          color: Colors.white,
+        ),
+        SizedBox(
+          width: 10,
+        ),
+      ],
+    );
+  }
+}
